930131732153b71aeac535f2f4ab3ea4693a8936
diogoakira <diogoakira@NBBV027450>
2019-12-03 11:01:43 -0300

    Add project files.

---
+++ Logging/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logging.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Teste";
+        }
+    }
+}
